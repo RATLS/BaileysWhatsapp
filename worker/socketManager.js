670361b1905ead5bce3ed0cbf3fe36d42e655754
Shardul Rasal <shardul@reachandteach.in>
2026-02-13 17:17:45 +0530

    code

--- worker/socketManager.js
+++ worker/socketManager.js
@@ -17,6 +17,7 @@ const redis = new Redis({
 const sockets = new Map()
 const connectedClients = new Set()
 const senderLoops = new Set()
+const initializingClients = new Set()
 
 
 // let isBooting = true
@@ -28,141 +29,154 @@ function publishEvent(event) {
 }
 
 async function initClient(clientId) {
-  // bootingClients.add(clientId)
-  await setClientState(clientId, STATES.CONNECTING)
-  if (sockets.has(clientId)) {
-    console.log(`⚠️ Client ${clientId} already initialized`)
-    return sockets.get(clientId)
+
+  if (initializingClients.has(clientId)) {
+    console.log(`⏳ ${clientId} already initializing`)
+    return
   }
 
-  const sessionPath = `/sessions/${clientId}`
+  initializingClients.add(clientId)
 
-  const { state, saveCreds } = await useMultiFileAuthState(sessionPath)
+  try{
+  // bootingClients.add(clientId)
+    await setClientState(clientId, STATES.CONNECTING)
 
-  const sock = makeWASocket({
-    auth: state,
-    // logger: Pino({ level: "debug" }),
-    logger: Pino({ level: "silent" }).child({level: "silent" }),
-    printQRInTerminal: false,
-    markOnlineOnConnect: false,
-    browser: ["Admissions - CRM", "Linux", "120.0.0"]
-  })
-
-  sockets.set(clientId, sock)
-
-  sock.ev.on("creds.update", saveCreds)
-
-  sock.ev.on("connection.update", async (update) => {
-    console.log(`📡 [${clientId}] connection.update`, JSON.stringify(update))
-
-    const { connection, qr, lastDisconnect } = update
-
-    if (qr) {
-      await setClientState(clientId, STATES.QR_REQUIRED)
-
-      // ✅ Save QR (important)
-      await redis.set(`wa:qr:${clientId}`, qr, "EX", 120)
-
-      publishEvent({
-        type: "qr",
-        clientId,
-        qr
-      })
-
-      // bootingClients.delete(clientId)
-      return
+    if (sockets.has(clientId)) {
+      console.log(`⚠️ Client ${clientId} already initialized`)
+      return sockets.get(clientId)
     }
 
-    if (connection === "open") {
-      console.log(`🟢 ${clientId} connection opened`)
+    const sessionPath = `/sessions/${clientId}`
 
-      await setClientState(clientId, STATES.CONNECTED)
+    const { state, saveCreds } = await useMultiFileAuthState(sessionPath)
 
-      await redis.del(`wa:qr:${clientId}`)
+    const sock = makeWASocket({
+      auth: state,
+      // logger: Pino({ level: "debug" }),
+      logger: Pino({ level: "silent" }).child({level: "silent" }),
+      printQRInTerminal: false,
+      markOnlineOnConnect: false,
+      browser: ["Admissions - CRM", "Linux", "120.0.0"]
+    })
 
-      publishEvent({
-        type: "status",
-        clientId,
-        state: "CONNECTED"
-      })
+    sockets.set(clientId, sock)
 
-      connectedClients.add(clientId)
+    sock.ev.on("creds.update", saveCreds)
 
-      // bootingClients.delete(clientId)
+    sock.ev.on("connection.update", async (update) => {
+      console.log(`📡 [${clientId}] connection.update`, JSON.stringify(update))
 
-      console.log(`✅ ${clientId} connected successfully`)
+      const { connection, qr, lastDisconnect } = update
 
-      setTimeout(() => {
-        startSenderLoop(clientId)
-      }, 2000)
+      if (qr) {
+        await setClientState(clientId, STATES.QR_REQUIRED)
 
-      return
-    }
+        // ✅ Save QR (important)
+        await redis.set(`wa:qr:${clientId}`, qr, "EX", 120)
 
-    if (connection === "close") {
-      connectedClients.delete(clientId)
-      const statusCode =
-        lastDisconnect?.error?.output?.statusCode ??
-        lastDisconnect?.error?.output?.payload?.statusCode
+        publishEvent({
+          type: "qr",
+          clientId,
+          qr
+        })
 
-      // if (statusCode === undefined && bootingClients.has(clientId)) {
-      //   console.log(`🟡 ${clientId} waiting for QR...`)
-      //   return
-      // }
+        // bootingClients.delete(clientId)
+        return
+      }
 
-      console.log(`❌ ${clientId} disconnected (${statusCode})`)
+      if (connection === "open") {
+        console.log(`🟢 ${clientId} connection opened`)
 
-      // 🚪 Logged out / Unauthorized
-      if (statusCode === DisconnectReason.loggedOut || statusCode === 401) {
-        await setClientState(clientId, STATES.LOGGED_OUT)
+        await setClientState(clientId, STATES.CONNECTED)
+
+        await redis.del(`wa:qr:${clientId}`)
 
         publishEvent({
           type: "status",
           clientId,
-          state: "LOGGED_OUT"
+          state: "CONNECTED"
         })
 
-        const oldSock = sockets.get(clientId)
+        connectedClients.add(clientId)
 
-        if (oldSock) {
-          oldSock.ev.removeAllListeners()
-          try { oldSock.end() } catch {}
-        }
-
-        sockets.delete(clientId)
-        clearSession(clientId)
-
-        console.log(`📲 ${clientId} requires new QR`)
         // bootingClients.delete(clientId)
 
-        // 🔥 Auto re-init to generate new QR
+        console.log(`✅ ${clientId} connected successfully`)
+
         setTimeout(() => {
-          console.log(`🔄 Reinitializing ${clientId} for new QR`)
-          initClient(clientId)
-        }, 1000)
+          startSenderLoop(clientId)
+        }, 2000)
 
         return
       }
 
-      // 🌐 Transient disconnect
-      await setClientState(clientId, STATES.DISCONNECTED)
-      publishEvent({
-        type: "status",
-        clientId,
-        state: "DISCONNECTED"
-      })
+      if (connection === "close") {
+        connectedClients.delete(clientId)
+        const statusCode =
+          lastDisconnect?.error?.output?.statusCode ??
+          lastDisconnect?.error?.output?.payload?.statusCode
 
-      sockets.delete(clientId)
+        // if (statusCode === undefined && bootingClients.has(clientId)) {
+        //   console.log(`🟡 ${clientId} waiting for QR...`)
+        //   return
+        // }
 
-      setTimeout(() => {
-        console.log(`🔄 Reconnecting ${clientId}...`)
-        initClient(clientId)
-      }, 5000)
+        console.log(`❌ ${clientId} disconnected (${statusCode})`)
+
+        // 🚪 Logged out / Unauthorized
+        if (statusCode === DisconnectReason.loggedOut || statusCode === 401) {
+          await setClientState(clientId, STATES.LOGGED_OUT)
+
+          publishEvent({
+            type: "status",
+            clientId,
+            state: "LOGGED_OUT"
+          })
+
+          const oldSock = sockets.get(clientId)
+
+          if (oldSock) {
+            oldSock.ev.removeAllListeners()
+            try { oldSock.end() } catch {}
+          }
+
+          sockets.delete(clientId)
+          clearSession(clientId)
+
+          console.log(`📲 ${clientId} requires new QR`)
+          // bootingClients.delete(clientId)
+
+          // 🔥 Auto re-init to generate new QR
+          setTimeout(() => {
+            console.log(`🔄 Reinitializing ${clientId} for new QR`)
+            initClient(clientId)
+          }, 1000)
+
+          return
+        }
+
+        // 🌐 Transient disconnect
+        await setClientState(clientId, STATES.DISCONNECTED)
+        publishEvent({
+          type: "status",
+          clientId,
+          state: "DISCONNECTED"
+        })
+
+        sockets.delete(clientId)
+
+        setTimeout(() => {
+          console.log(`🔄 Reconnecting ${clientId}...`)
+          initClient(clientId)
+        }, 5000)
+      }
+    })
+
+    // console.log('sockets after update: ',sockets)
+    return sock
+  }finally {
+      initializingClients.delete(clientId)
     }
-  })
-
-  // console.log('sockets after update: ',sockets)
-  return sock
 }
 
 function sleep(ms) {
